73e2dae34a643c307bcc183894caaa8d5874844f
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-27 03:52:54 +0200

    Added icons.

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,12 +1,20 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="28" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
+      <SideBarIcon icon={<FaPoo size="28" />} />
+      <SideBarIcon icon={<BsGearFill size="28" />} />
     </div>
   );
 };
+
+const SideBarIcon = ({ icon }) => {
+  return <div className="sidebar-icon">{icon}</div>;
+};
+
 export default SideBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+      },
+    },
   },
   plugins: [],
 };
