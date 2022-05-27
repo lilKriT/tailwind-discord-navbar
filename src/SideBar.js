0e8ab692d3803eb501e67f82a1e28f26b95d02a9
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-27 03:58:06 +0200

    Styled the buttons using custom class.

--- src/SideBar.js
+++ src/SideBar.js
@@ -5,10 +5,10 @@ const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
       <SideBarIcon icon={<FaFire size="28" />} />
-      <SideBarIcon icon={<BsPlus size="28" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="28" />} />
-      <SideBarIcon icon={<FaPoo size="28" />} />
-      <SideBarIcon icon={<BsGearFill size="28" />} />
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+      <SideBarIcon icon={<BsGearFill size="22" />} />
     </div>
   );
 };
